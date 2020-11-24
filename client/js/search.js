06a96d9c4d50efa9c24df067596ab999e848a160
Agustin Pitaro <agustin.pitaro@gmail.com>
2020-11-24 12:13:35 -0300

    Solucionado el caso de buscar algo que no este en la biblioteca

--- client/js/search.js
+++ client/js/search.js
@@ -6,14 +6,14 @@ predict.appendChild(predictLI);
 
 buscador.addEventListener('keyup', function (e) {
     if (e.code === 'Enter') {
-        debugger;
         let productPage = "product.html?link=" + miniSearch.search(miniSearch.autoSuggest(buscador.value)[0].suggestion, { fuzzy: 0.2 })[0].link;
         window.location.href = productPage;
     }
     else {
-        if (buscador.value.length > 3) {
+        if (buscador.value.length > 3 && miniSearch.search(miniSearch.autoSuggest(e.target.value)[0].suggestion, { fuzzy: 0.2 }) ) {
             predict.style.display = "block";
-            predictLI.innerHTML = miniSearch.search(miniSearch.autoSuggest(e.target.value)[0].suggestion, { fuzzy: 0.2 })[0].titulo;
+            let resultado = miniSearch.search(miniSearch.autoSuggest(e.target.value)[0].suggestion, { fuzzy: 0.2 })[0].titulo;
+            predictLI.innerHTML = resultado;
         }
         else {
             predictLI.innerHTML = '';
