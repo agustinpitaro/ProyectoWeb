8a50c95333bb14d276e71173796c30b1bbea2f26
Agustin Pitaro <agustin.pitaro@gmail.com>
2020-12-14 23:57:24 -0300

    index terminado

--- src/catalogo/catalogo.service.ts
+++ src/catalogo/catalogo.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Product } from '../producto';
-import * as fs from 'fs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Producto } from 'src/producto/producto.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CatalogoService {
-    getProductos(): Product[] {
-        let productos = fs.readFileSync('resources/productos.csv', 'utf8');
-        const elementosP = productos.split('\n')
-            .map(p => p.replace('\r', '')).map(p => p.split(','));
-        let listaProductos: Product[] = [];
-        for (let i = 0; i < elementosP.length; i++) {
-            let producto = new Product(parseInt(elementosP[i][0]), elementosP[i][1], elementosP[i][2], elementosP[i][3], elementosP[i][4], elementosP[i][5],elementosP[i][6], elementosP[i][7]);                 
-                listaProductos.push(producto);
-        }
-        return listaProductos;
+    constructor(
+        @InjectRepository(Producto)
+        private readonly productoRepository: Repository<Producto>
+    ) { }
+    public async getProductos(): Promise<Producto[]> {
+        let recientes = await this.productoRepository.find();
+        return recientes;
     }
 }
