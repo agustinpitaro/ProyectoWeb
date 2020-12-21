87075d309ba33a39e682ecab81937988663355ca
Agustin Pitaro <agustin.pitaro@gmail.com>
2020-12-20 23:34:49 -0300

    Acomodado esquema sql

--- esquema_proyectoWEB.sql
+++ esquema_proyectoWEB.sql
@@ -85,7 +85,7 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `nro_usuario` int NOT NULL AUTO_INCREMENT,
   `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`nro_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
