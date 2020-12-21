60427ecb35f01c8255f6f6b66b2b2b6a31f45553
Dardo Facundo Fernández Lhomy <dardoflm@gmail.com>
2020-12-20 23:03:40 -0300

    register encriptado

--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -14,10 +14,11 @@ export class RegisterService {
     public async register(userInfo: any): Promise<boolean> {
         const bcrypt = require('bcrypt');
         const saltRounds = 10;     
-        if (await this.usuarioRepository.findOne(userInfo.name)) {
+        if (await this.usuarioRepository.find({
+            where : [ {"username": userInfo.username}]})) {
             return false;
         }
-        let salt = bcrypt.genSaltSync(10);
+        let salt = bcrypt.genSaltSync(saltRounds);
         let hash = bcrypt.hashSync(userInfo.password, salt);
         let userRegister = new Usuario(userInfo.name, hash);
         console.log(userRegister);
