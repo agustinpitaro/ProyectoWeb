f7b0e2bb1204da9dfc0436b575043ac582ff63a2
Agustin Pitaro <agustin.pitaro@gmail.com>
2020-12-21 12:43:08 -0300

    Fixed getPopulares

--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Biblioteca } from 'src/biblioteca/biblioteca.entity';
 import { Producto } from 'src/producto/producto.entity';
+import { ProductoService } from 'src/producto/producto.service';
 import { Usuario } from 'src/users/users.entity';
 import { IndexController } from './index.controller';
 import { IndexService } from './index.service';
@@ -13,7 +14,7 @@ import { IndexService } from './index.service';
         Biblioteca,
         Usuario
       ])],
-    providers: [IndexService],
+    providers: [IndexService, ProductoService],
     controllers: [IndexController]
   })
 export class IndexModule {}
--- src/index/index.service.ts
+++ src/index/index.service.ts
@@ -4,38 +4,53 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Producto } from 'src/producto/producto.entity';
 import { getConnection, Repository } from 'typeorm';
 import { Biblioteca } from 'src/biblioteca/biblioteca.entity';
+import { UsersService } from 'src/users/users.service';
+import { ProductoService } from 'src/producto/producto.service';
 
 @Injectable()
 export class IndexService {
     constructor(
+        public ProductoService:ProductoService,
         @InjectRepository(Producto)
-        private readonly productoRepository: Repository<Producto>
-    ) { }
+        private readonly productoRepository: Repository<Producto>,
+        @InjectRepository(Biblioteca)
+        private readonly bibliotecaRepository: Repository<Biblioteca>
+    ) {}
 
     public async getProductosPopulares(): Promise<Producto[]> {
+        let lista = await this.productoRepository.find();
+        let listaPuntaje = [];
+        for(let i = 0; i < lista.length ; i++){
+            listaPuntaje[i] = {
+                                nro_producto: lista[i].nro_producto,
+                                puntaje: await this.ProductoService.getPuntaje(lista[i].nro_producto)
+                            } 
+        }
+        listaPuntaje = listaPuntaje.sort((obj1, obj2) => {
+            if (obj1.puntaje > obj2.puntaje) {
+                return -1;
+            }
         
-        let listafinal = await this.productoRepository.find({take : 3});
-        /*
-        let lista = await getConnection()
-        .createQueryBuilder()
-        .select("nro_producto, AVG(puntaje)")
-        .from(Biblioteca, "tuplas").orderBy("AVG(puntaje)", "DESC")
-        .limit(5).getMany();
-
-        console.log(lista);
-        let listafinal : Producto[];
-        lista.forEach(async p => {
-            let producto = await this.productoRepository.findOne(p.getNroProducto());
-            listafinal.push(producto);
+            if (obj1.puntaje < obj2.puntaje) {
+                return 1;
+            }
+            return 0;
         });
-        */
-        return listafinal;
+        let cantidadPopulares = 5;
+        let listaFinal: Producto[] = [];
+        for(let i = 0 ; i < cantidadPopulares; i++){
+            let producto = await this.productoRepository.findOne(listaPuntaje[i].nro_producto);
+            listaFinal.push(producto);
+        }
+        return listaFinal;
     }
+
     public async getProductosCarousel(): Promise<Producto[]> {
 
         let carousel = await this.productoRepository.find({take : 3});
         return carousel;
     }
+    
     public async getProductosRecientes(): Promise<Producto[]> {
         let recientes = await this.productoRepository.find({take : 5});
         return recientes;
